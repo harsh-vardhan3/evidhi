dd416ff15853805c5ea36a8f20e1634b31e50340
vermaaakriti215 <138132347+vermaaakriti215@users.noreply.github.com>
2023-12-20 07:23:24 +0530

    Update faq.js

--- websitex/faq.js
+++ websitex/faq.js
@@ -1,7 +1,287 @@
-const faqs= document.querySelectorAll(".faq");
+const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach((faq) => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
-    });
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
 });
+const data = {
+  currentUser: {
+    image: {
+      png: "./images/avatars/image-juliusomo.png",
+      webp: "./images/avatars/image-juliusomo.webp",
+    },
+    username: "sooraj_adv",
+  },
+  comments: [
+    {
+      parent: 0,
+      id: 1,
+      content:
+        "How can I protect my intellectual property rights for a new invention?",
+      createdAt: "1 month ago",
+      score: 12,
+      user: {
+        image: {
+          png: "./images/avatars/image-amyrobson.png",
+          webp: "./images/avatars/image-amyrobson.webp",
+        },
+        username: "prakashraj",
+      },
+      replies: [
+        {
+          parent: 2,
+          id: 1,
+          content:
+            "To protect your invention, consider filing for a patent. Consult with a patent attorney who can guide you through the application process and help ensure your intellectual property is safeguarded.",
+          createdAt: "1 week ago",
+          score: 4,
+          replyingTo: "prakashraj",
+          user: {
+            image: {
+              png: "./images/avatars/image-ramsesmiron.png",
+              webp: "./images/avatars/image-ramsesmiron.webp",
+            },
+            username: "dipariya",
+          },
+        },
+      ],
+    },
+
+    {
+      parent: 0,
+      id: 2,
+      content: "What steps should I take if I'm facing criminal charges?",
+      createdAt: "2 weeks ago",
+      score: 5,
+      user: {
+        image: {
+          png: "./images/avatars/image-maxblagun.png",
+          webp: "./images/avatars/image-maxblagun.webp",
+        },
+        username: "sharma_rd",
+      },
+      replies: [
+        {
+          parent: 2,
+          id: 1,
+          content:
+            "If facing criminal charges, it's crucial to consult with a criminal defense attorney immediately. They can advise you on your rights, build a defense strategy, and represent you in court to ensure a fair legal process.",
+          createdAt: "1 week ago",
+          score: 4,
+          replyingTo: "sharma_rd",
+          user: {
+            image: {
+              png: "./images/avatars/image-ramsesmiron.png",
+              webp: "./images/avatars/image-ramsesmiron.webp",
+            },
+            username: "dipariya",
+          },
+        },
+        {
+          parent: 2,
+          id: 1,
+          content:
+            "Your first step should be to retain an experienced criminal defense attorney who has handled cases where the accused faced similar criminal charges as yours. The importance of hiring a lawyer immediately—even if you are still just a suspect—cannot be emphasized enough.",
+          createdAt: "2 days ago",
+          score: 2,
+          replyingTo: "sharma_rd",
+          user: {
+            image: {
+              png: "./images/avatars/image-juliusomo.png",
+              webp: "./images/avatars/image-juliusomo.webp",
+            },
+            username: "Kshitij",
+          },
+        },
+      ],
+    },
+    {
+      parent: 0,
+      id: 3,
+      content: "How does the probate process work?",
+      createdAt: "1 week ago",
+      score: 5,
+      user: {
+        image: {
+          png: "./images/avatars/image-maxblagun.png",
+          webp: "./images/avatars/image-maxblagun.webp",
+        },
+        username: "sg_gurudev",
+      },
+      replies: [],
+    },
+    {
+      parent: 0,
+      id: 4,
+      content: "What rights do tenants have in eviction situations?",
+      createdAt: "5 days ago",
+      score: 5,
+      user: {
+        image: {
+          png: "./images/avatars/image-maxblagun.png",
+          webp: "./images/avatars/image-maxblagun.webp",
+        },
+        username: "vidyalaxmi",
+      },
+      replies: [],
+    },
+  ],
+};
+function appendFrag(frag, parent) {
+  var children = [].slice.call(frag.childNodes, 0);
+  parent.appendChild(frag);
+  //console.log(children);
+  return children[1];
+}
+
+const addComment = (body, parentId, replyTo = undefined) => {
+  let commentParent =
+    parentId === 0
+      ? data.comments
+      : data.comments.filter((c) => c.id == parentId)[0].replies;
+  let newComment = {
+    parent: parentId,
+    id:
+      commentParent.length == 0
+        ? 1
+        : commentParent[commentParent.length - 1].id + 1,
+    content: body,
+    createdAt: "Now",
+    replyingTo: replyTo,
+    score: 0,
+    replies: parent == 0 ? [] : undefined,
+    user: data.currentUser,
+  };
+  commentParent.push(newComment);
+  initComments();
+};
+const deleteComment = (commentObject) => {
+  if (commentObject.parent == 0) {
+    data.comments = data.comments.filter((e) => e != commentObject);
+  } else {
+    data.comments.filter((e) => e.id === commentObject.parent)[0].replies =
+      data.comments
+        .filter((e) => e.id === commentObject.parent)[0]
+        .replies.filter((e) => e != commentObject);
+  }
+  initComments();
+};
+
+const promptDel = (commentObject) => {
+  const modalWrp = document.querySelector(".modal-wrp");
+  modalWrp.classList.remove("invisible");
+  modalWrp.querySelector(".yes").addEventListener("click", () => {
+    deleteComment(commentObject);
+    modalWrp.classList.add("invisible");
+  });
+  modalWrp.querySelector(".no").addEventListener("click", () => {
+    modalWrp.classList.add("invisible");
+  });
+};
+
+const spawnReplyInput = (parent, parentId, replyTo = undefined) => {
+  if (parent.querySelectorAll(".reply-input")) {
+    parent.querySelectorAll(".reply-input").forEach((e) => {
+      e.remove();
+    });
+  }
+  const inputTemplate = document.querySelector(".reply-input-template");
+  const inputNode = inputTemplate.content.cloneNode(true);
+  const addedInput = appendFrag(inputNode, parent);
+  addedInput.querySelector(".bu-primary").addEventListener("click", () => {
+    let commentBody = addedInput.querySelector(".cmnt-input").value;
+    if (commentBody.length == 0) return;
+    addComment(commentBody, parentId, replyTo);
+  });
+};
+
+const createCommentNode = (commentObject) => {
+  const commentTemplate = document.querySelector(".comment-template");
+  var commentNode = commentTemplate.content.cloneNode(true);
+  commentNode.querySelector(".usr-name").textContent =
+    commentObject.user.username;
+  commentNode.querySelector(".usr-img").src = commentObject.user.image.webp;
+  commentNode.querySelector(".score-number").textContent = commentObject.score;
+  commentNode.querySelector(".cmnt-at").textContent = commentObject.createdAt;
+  commentNode.querySelector(".c-body").textContent = commentObject.content;
+  if (commentObject.replyingTo)
+    commentNode.querySelector(".reply-to").textContent =
+      "@" + commentObject.replyingTo;
+
+  commentNode.querySelector(".score-plus").addEventListener("click", () => {
+    commentObject.score++;
+    initComments();
+  });
+
+  commentNode.querySelector(".score-minus").addEventListener("click", () => {
+    commentObject.score--;
+    if (commentObject.score < 0) commentObject.score = 0;
+    initComments();
+  });
+  if (commentObject.user.username == data.currentUser.username) {
+    commentNode.querySelector(".comment").classList.add("this-user");
+    commentNode.querySelector(".delete").addEventListener("click", () => {
+      promptDel(commentObject);
+    });
+    commentNode.querySelector(".edit").addEventListener("click", (e) => {
+      const path = e.path[3].querySelector(".c-body");
+      if (
+        path.getAttribute("contenteditable") == false ||
+        path.getAttribute("contenteditable") == null
+      ) {
+        path.setAttribute("contenteditable", true);
+        path.focus();
+      } else {
+        path.removeAttribute("contenteditable");
+      }
+    });
+    return commentNode;
+  }
+  return commentNode;
+};
+
+const appendComment = (parentNode, commentNode, parentId) => {
+  const bu_reply = commentNode.querySelector(".reply");
+  // parentNode.appendChild(commentNode);
+  const appendedCmnt = appendFrag(commentNode, parentNode);
+  const replyTo = appendedCmnt.querySelector(".usr-name").textContent;
+  bu_reply.addEventListener("click", () => {
+    if (parentNode.classList.contains("replies")) {
+      spawnReplyInput(parentNode, parentId, replyTo);
+    } else {
+      //console.log(appendedCmnt.querySelector(".replies"));
+      spawnReplyInput(
+        appendedCmnt.querySelector(".replies"),
+        parentId,
+        replyTo
+      );
+    }
+  });
+};
+
+function initComments(
+  commentList = data.comments,
+  parent = document.querySelector(".comments-wrp")
+) {
+  parent.innerHTML = "";
+  commentList.forEach((element) => {
+    var parentId = element.parent == 0 ? element.id : element.parent;
+    const comment_node = createCommentNode(element);
+    if (element.replies && element.replies.length > 0) {
+      initComments(element.replies, comment_node.querySelector(".replies"));
+    }
+    appendComment(parent, comment_node, parentId);
+  });
+}
+
+initComments();
+const cmntInput = document.querySelector(".reply-input");
+cmntInput.querySelector(".bu-primary").addEventListener("click", () => {
+  let commentBody = cmntInput.querySelector(".cmnt-input").value;
+  if (commentBody.length == 0) return;
+  addComment(commentBody, 0);
+  cmntInput.querySelector(".cmnt-input").value = "";
+});
+
+// addComment("Hello ! It works !!",0);
